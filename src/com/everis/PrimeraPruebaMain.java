53b2f04dd69446326ce8ac95d6ce694c66a37723
Elias <eespinoh@everis.com>
2021-03-24 13:48:33 +0100

    Version 1 - Git

---
+++ src/com/everis/PrimeraPruebaMain.java
@@ -0,0 +1,20 @@
+package com.everis;
+
+/**
+ * 
+ * @author eespinoh
+ *
+ */
+
+public class PrimeraPruebaMain {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
